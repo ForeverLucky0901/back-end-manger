4c3711b3db8a52969ee9365113a26219b2ababfe
244552058@qq.com <244552058@qq.com>
2022-06-24 17:03:23 +0800

    新增修改

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { createVuePlugin } from 'vite-plugin-vue2'
 // import { loadEnv } from 'vite'
 // const env = loadEnv(mode, process.cwd(), 'ENV');
 export default {
-    plugins: [createVuePlugin()],
+    plugins: [createVuePlugin()]
 
 }
