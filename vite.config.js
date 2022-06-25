59784ea3bff806cdf15f6de4e65a2a96a892112e
244552058@qq.com <244552058@qq.com>
2022-06-25 10:04:54 +0800

    新增配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { createVuePlugin } from 'vite-plugin-vue2'
+import { defineConfig } from 'vite'
 // import { loadEnv } from 'vite'
 // const env = loadEnv(mode, process.cwd(), 'ENV');
-export default {
-    plugins: [createVuePlugin()]
+export default defineConfig(({ command, mode }) => {
 
-}
+    // if (command === 'serve') {
+    //     return {
+    //         // dev 独有配置
+    //     }
+    // } else {
+    //     // command === 'build'
+    //     return {
+    //         // build 独有配置
+    //     }
+    // }
+
+})
